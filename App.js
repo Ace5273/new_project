2e40d1757cfb9ab71e5f5d6e272d54ecbc6bec65
Ace <sagica5273@gmail.com>
2017-10-05 23:30:32 +0300

    eaascda

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-            <Text>Somthing</Text>
+            <Text>Hii</Text>
             <TextInput />
       </View>
     );
